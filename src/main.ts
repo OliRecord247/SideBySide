49a2f2e431d3e01f08373d23e331f1e9630d0dc6
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-04-07 17:54:17 +0200

    Setup API with fastify and plugin for DI

---
+++ src/main.ts
@@ -0,0 +1,68 @@
+import Fastify from 'fastify';
+import { MikroORM } from '@mikro-orm/postgresql';
+import { diContainer, fastifyAwilixPlugin } from '@fastify/awilix';
+import { asClass, asValue, Lifetime } from 'awilix';
+
+import { UserService } from './services/user.service.js';
+import config from './mikro-orm.config.js';
+
+// const orm = await MikroORM.init(config);
+const app = Fastify({ logger: true });
+const orm = await MikroORM.init(config);
+
+async function run() {
+    app.register(fastifyAwilixPlugin, {
+        disposeOnClose: true,
+        disposeOnResponse: true,
+        strictBooleanEnforced: true
+    });
+
+    diContainer.register({
+        userService: asClass(UserService, { lifetime: Lifetime.SCOPED }),
+        orm: asValue(orm),
+    });
+
+    app.addHook('onRequest', async (request) => {
+        const scopedEm = orm.em.fork();
+
+        request.diScope.register({
+            em: asValue(scopedEm),
+        });
+    });
+
+    app.get('/users', async (request) => {
+        const userService = request.diScope.resolve<UserService>('userService');
+        const users = await userService.findAll();
+        return users;
+    });
+
+    app.get('/users/:id', async (request) => {
+        const { id } = request.params as { id: string };
+        const userService = request.diScope.resolve<UserService>('userService');
+        const user = await userService.findById(id);
+        return user;
+    });
+
+    app.post('/users', async (request) => {
+        const userService = request.diScope.resolve<UserService>('userService');
+        const user = await userService.create(request.body as any);
+        return { success: true, user };
+    });
+
+    app.delete("/users/:id", async (request) => {
+        const { id } = request.params as { id: string };
+        const userService = request.diScope.resolve<UserService>('userService');
+        await userService.deleteById(id);
+        return { success: true };
+    });
+
+    try {
+        await app.listen({ port: 3000, host: '0.0.0.0' });
+        console.log("🚀 Server is ready op http://localhost:3000");
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+}
+
+await run();
