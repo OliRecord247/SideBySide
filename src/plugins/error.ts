89d89f11f284bdd034416195bee439fdb30066d2
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-04-24 15:05:30 +0200

    Rewrite handling errors

--- src/plugins/error.ts
+++ src/plugins/error.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { z } from 'zod';
+import { HttpError } from '@fastify/sensible';
 import { validatorCompiler, serializerCompiler, hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod';
 
 export async function setupValidator(app: FastifyInstance) {
@@ -8,7 +8,6 @@ export async function setupValidator(app: FastifyInstance) {
 
     app.setErrorHandler((error, request, reply) => {
         if (hasZodFastifySchemaValidationErrors(error)) {
-            console.log(error);
             return reply.status(400).send({
                 success: false,
                 message: "Validation failed",
@@ -19,13 +18,14 @@ export async function setupValidator(app: FastifyInstance) {
             });
         }
 
+        if (error instanceof HttpError) {
+            return reply.status(error.statusCode).send({
+                success: false,
+                message:  error.statusCode < 500 ? error.message : "Interne server fout"
+            });
+        }
+
         request.log.error(error);
-
-        // reply.status(error.statusCode || 500).send({
-        //     statusCode: error.statusCode || 500,
-        //     message: error.message || 'Interne server fout',
-        // });
-
         reply.status(500).send({
             success: false,
             message: "Interne server fout",
